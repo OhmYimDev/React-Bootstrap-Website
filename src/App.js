2d5e65e2dad7149ed82d9718f4c01087d838d618
OhmYimDev <phongphan_dw@hotmail.com>
2022-04-10 16:33:05 +0700

    Services & Portfolio

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './App.css';
 import Typed from 'react-typed';
+import Script from './script';
 
 function App() {
   return (
     <div className='App' id='page-top'>
       {/* Navigation */}
       <div className='bg-img'>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-          <div class="container">
-            <a class="navbar-brand fs-2" href="#page-top">Start Bootstrap</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id='mainNav'>
+          <div className="container">
+            <a className="navbar-brand fs-2" href="#page-top">Start Bootstrap</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               MENU
               <FontAwesomeIcon className='ms-1' icon={faBars}/>
             </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-              <ul class="navbar-nav text-uppercase my-3">
-                <li class="nav-item ">
-                  <a class="nav-link" href="#">services</a>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <ul className="navbar-nav text-uppercase my-3">
+                <li className="nav-item ">
+                  <a className="nav-link" href="#">services</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">portfolio</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">portfolio</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">about</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">about</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">team</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">team</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">contact</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">contact</a>
                 </li>
               </ul>
             </div>
@@ -56,8 +62,154 @@ function App() {
         <section className='page-section' id='services' >
           <div className='container'>
             <div className='text-center'>
-              <h2 className='section-heading' >Services</h2>
-              <h3 className='section-subheading'>Lorem ipsum dolor sit amet consectetur.</h3>
+              <div className='section-heading' >Services</div>
+              <div className='section-subheading'>Lorem ipsum dolor sit amet consectetur.</div>
+            </div>
+            <div className='row text-center'>
+              <div className='col-md-4'>
+                <span className='fa-stack fa-4x'>
+                  <FontAwesomeIcon className='fa-stack-2x' icon={faCircle} />
+                  <FontAwesomeIcon className='fa-stack-1x fa-inverse' icon={faCartShopping} />
+                </span>
+                <h4 className='my-3'>E-Commerce</h4>
+                <p className='text-muted'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+              </div>
+              <div className='col-md-4'>
+                <span className='fa-stack fa-4x'>
+                  <FontAwesomeIcon className='fa-stack-2x' icon={faCircle} />
+                  <FontAwesomeIcon className='fa-stack-1x fa-inverse' icon={faLaptopCode} />
+                </span>
+                <h4 className='my-3'>Responsive Design</h4>
+                <p className='text-muted'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+              </div>
+              <div className='col-md-4'>
+                <span className='fa-stack fa-4x'>
+                  <FontAwesomeIcon className='fa-stack-2x' icon={faCircle} />
+                  <FontAwesomeIcon className='fa-stack-1x fa-inverse' icon={faLock} />
+                </span>
+                <h4 className='my-3'>Web Security</h4>
+                <p className='text-muted'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* Portfolio Grid */}
+        <section className='page-section bg-light' id='portfolio'>
+          <div className='container'>
+            <div className='text-center'>
+              <h2 className='section-heading text-uppercase'>Portfolio</h2>
+              <h3 className='section-subheading text-muted'>Lorem ipsum dolor sit amet consectetur.</h3>
+            </div>
+            <div className='row'>
+
+              <div className='col-lg-4 col-sm-6 mb-4'>
+                {/* Portfolio item 1 */}
+                <div className='portfolio-item'>
+                  <a className='portfolio-link' data-bs-toggle="modal" href='#portfolioModal1'>
+                    <div className='portfolio-hover'>
+                      <div className='portfolio-hover-content'>
+                        <FontAwesomeIcon icon={faPlus}/>
+                      </div>
+                    </div>
+                    <img className='img-fluid' src={require("./assets/img/portfolio/1.jpg")} alt=''/>
+                  </a>
+                  <div className='portfolio-caption'>
+                    <div className='portfolio-caption-heading'>Threads</div>
+                    <div className='portfolio-caption-subheading text-muted'>Illustration</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-lg-4 col-sm-6 mb-4'>
+                {/* Portfolio item 2 */}
+                <div className='portfolio-item'>
+                  <a className='portfolio-link' data-bs-toggle="modal" href='#portfolioModal1'>
+                    <div className='portfolio-hover'>
+                      <div className='portfolio-hover-content'>
+                        <FontAwesomeIcon icon={faPlus}/>
+                      </div>
+                    </div>
+                    <img className='img-fluid' src={require("./assets/img/portfolio/2.jpg")} alt=''/>
+                  </a>
+                  <div className='portfolio-caption'>
+                    <div className='portfolio-caption-heading'>Threads</div>
+                    <div className='portfolio-caption-subheading text-muted'>Illustration</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-lg-4 col-sm-6 mb-4'>
+                {/* Portfolio item 3 */}
+                <div className='portfolio-item'>
+                  <a className='portfolio-link' data-bs-toggle="modal" href='#portfolioModal1'>
+                    <div className='portfolio-hover'>
+                      <div className='portfolio-hover-content'>
+                        <FontAwesomeIcon icon={faPlus}/>
+                      </div>
+                    </div>
+                    <img className='img-fluid' src={require("./assets/img/portfolio/3.jpg")} alt=''/>
+                  </a>
+                  <div className='portfolio-caption'>
+                    <div className='portfolio-caption-heading'>Threads</div>
+                    <div className='portfolio-caption-subheading text-muted'>Illustration</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-lg-4 col-sm-6 mb-4'>
+                {/* Portfolio item 4 */}
+                <div className='portfolio-item'>
+                  <a className='portfolio-link' data-bs-toggle="modal" href='#portfolioModal1'>
+                    <div className='portfolio-hover'>
+                      <div className='portfolio-hover-content'>
+                        <FontAwesomeIcon icon={faPlus}/>
+                      </div>
+                    </div>
+                    <img className='img-fluid' src={require("./assets/img/portfolio/4.jpg")} alt=''/>
+                  </a>
+                  <div className='portfolio-caption'>
+                    <div className='portfolio-caption-heading'>Threads</div>
+                    <div className='portfolio-caption-subheading text-muted'>Illustration</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-lg-4 col-sm-6 mb-4'>
+                {/* Portfolio item 5 */}
+                <div className='portfolio-item'>
+                  <a className='portfolio-link' data-bs-toggle="modal" href='#portfolioModal1'>
+                    <div className='portfolio-hover'>
+                      <div className='portfolio-hover-content'>
+                        <FontAwesomeIcon icon={faPlus}/>
+                      </div>
+                    </div>
+                    <img className='img-fluid' src={require("./assets/img/portfolio/5.jpg")} alt=''/>
+                  </a>
+                  <div className='portfolio-caption'>
+                    <div className='portfolio-caption-heading'>Threads</div>
+                    <div className='portfolio-caption-subheading text-muted'>Illustration</div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-lg-4 col-sm-6 mb-4'>
+                {/* Portfolio item 6 */}
+                <div className='portfolio-item'>
+                  <a className='portfolio-link' data-bs-toggle="modal" href='#portfolioModal1'>
+                    <div className='portfolio-hover'>
+                      <div className='portfolio-hover-content'>
+                        <FontAwesomeIcon icon={faPlus}/>
+                      </div>
+                    </div>
+                    <img className='img-fluid' src={require("./assets/img/portfolio/6.jpg")} alt=''/>
+                  </a>
+                  <div className='portfolio-caption'>
+                    <div className='portfolio-caption-heading'>Threads</div>
+                    <div className='portfolio-caption-subheading text-muted'>Illustration</div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
---
+++ src/script.js
@@ -0,0 +1,19 @@
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+
